b43e4f0fcc043f4e0a29c4546ea9ca188235deff
Anierin Bliss <anierin.t.bliss@gmail.com>
2019-05-16 00:49:03 -0700

    Include vendor/pixelstyle

--- aosp_ether.mk
+++ aosp_ether.mk
@@ -14,6 +14,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+#Pixel-Style
+$(call inherit-product, vendor/pixelstyle/config.mk)
+
 #GApps
 TARGET_GAPPS_ARCH := arm64
 
